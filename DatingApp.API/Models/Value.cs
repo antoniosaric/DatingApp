725710149d70c9184851090dd5f19261e9b0f640
Tony Saric <saric.tony@gmail.com>
2019-07-12 17:50:51 -0700

    initial commit

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
